6969b9eea35e17e9364630b41590476c18298b79
Alexey Solovyov <me@alexeyslvv.ru>
2022-10-05 20:08:13 +0300

    add B solution

--- B/python/code.py
+++ B/python/code.py
@@ -1,6 +1,12 @@
 def get_card_count(n, k, cards) -> int:
     # your code goes here
-    return 0
+    s_max = s_left = sum(cards[:k])
+    s_right = 0
+    for i in range(1, k+1):
+        s_left -= cards[k-i]
+        s_right += cards[n-i]
+        s_max = max(s_max, s_left+s_right)
+    return s_max
 
 
 n = int(input())
